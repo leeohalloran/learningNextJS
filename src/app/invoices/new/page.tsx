b9e03662ebad76b00fb2fb349afde51baf680700
leeohalloran <lee_ohalloran@hotmail.com>
2025-02-16 11:15:09 +0000

    some more stuff

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,27 +1,41 @@
 
-import Link from "next/link"
-import { CirclePlus } from 'lucide-react';
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label"
+import {Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import {Button} from "@/components/ui/button"
+
 
 
 export default function Home() {
     return (
-        <main className="flex flex-col justify-center h-full text-center max-w-5xl mx-auto gap-6 my-12">
+        <main className="flex flex-col justify-center h-full max-w-5xl mx-auto gap-6 my-12">
             <div className="flex justify-between">
-                <h1 className="text-3xl font-semibold">
+                <h1 className="text-3xl font-semibold text-sm mb-2">
                     Create Invoice
                 </h1>
             </div>
+
+            <form className="grid gap-4  max-w-sm">
+                <div>
+                    <Label htmlFor="name">Billing Name</Label>
+                    <Input name="name" id="name"  className="block font-semibold text-sm mb-2" type="text" placeholder="Billing Name" />
+                </div>
+                <div>
+                    <Label htmlFor="email">Billing Email </Label>
+                    <Input name="email" id="email"  className="block font-semibold text-sm mb-2" type="email" placeholder="Billing Email" />
+                </div>
+                <div>
+                    <Label htmlFor="value">Value (£) </Label>
+                    <Input name="value" id="value"  className="block font-semibold text-sm mb-2" type="text" placeholder="Value" />
+                </div>
+                <div>
+                    <Label htmlFor="description">Description </Label>
+                    <Textarea name="description" id="description"  className="block font-semibold text-sm mb-2" />
+                </div>
+                <div>
+                    <Button className="w-full font-semibold">Submit</Button>
+                </div>
+            </form>
         </main>
     );
 }
